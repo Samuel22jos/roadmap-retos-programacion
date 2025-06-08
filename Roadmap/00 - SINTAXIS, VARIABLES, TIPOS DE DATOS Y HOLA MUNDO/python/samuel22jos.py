81b9e2c67eeae6d4532275b09463faaf10249d30
Samuel22jos <133303599+Samuel22jos@users.noreply.github.com>
2025-06-07 19:49:39 -0600

    samuel22jos.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/samuel22jos.py
@@ -0,0 +1,40 @@
+https://python.org
+# comentario en una linea
+
+"""
+esto tambien 
+es un comentario 
+varias lineas
+"""
+
+'''
+esto tambien 
+es un comentario 
+varias lineas
+'''
+
+my_variable = "mi variable"
+my_variable = "esta es mi nueva variable"
+
+MY_CONSTANT = "mi constante"# por convencion
+
+my_int  = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡hola, python!")
+ 
+
+
+
+
+
+
+
+
+
+
+
